4c0c5af5ae80adfe4493bfd821b400611dc851ce
LOPES Anderson <andlopes@europe.corp.altran.com>
2019-05-01 09:43:22 +0100

    Getting a single resource.

--- src/main/java/com/lopes/springbootquickstart/topic/TopicService.java
+++ src/main/java/com/lopes/springbootquickstart/topic/TopicService.java
@@ -7,12 +7,16 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
-	private List<Topic> topics = Arrays.asList(new Topic("Sping", "Spring Framework", "Spring Framework Description"),
+	private List<Topic> topics = Arrays.asList(new Topic("Spring", "Spring Framework", "Spring Framework Description"),
 			new Topic("Java", "Core Java", "Core java Description"),
 			new Topic("javascript", "javascript", "javascript Description"));
 
 	public List<Topic> getAllTopics() {
 		return topics;
+	}
+
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equalsIgnoreCase(id)).findFirst().get();
 	}
 
 }
